cbc2ffe38362377aa9a8d1e78db028c066972836
dbhor <dbhor1000@gmail.com>
2023-03-08 21:47:01 +0300

    Initial commit

---
+++ src/main/java/com/example/homework28proper2/Homework28proper2Application.java
@@ -0,0 +1,13 @@
+package com.example.homework28proper2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework28proper2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework28proper2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homework28proper2/Homework28proper2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework28proper2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework28proper2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
